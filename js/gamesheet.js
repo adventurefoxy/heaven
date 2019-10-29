e8fb321f84ef349cecd3424d6e3615e4ad7bb44a
Adventure <mileshenrique@gmail.com>
2019-10-29 20:43:01 -0200

    First

---
+++ js/gamesheet.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+function startop1() {
+    var name = document.getElementById('name_input').value;
+    if (name == '') {
+        alert('Please, insert a name.');
+    }
+}
---
+++ js/gamesheet.ts
@@ -0,0 +1,6 @@
+function startop1(){
+    let name:string = getElementById('name_input').value;
+    if (name == ''){
+        alert('Please, insert a name.');
+    }
+}
