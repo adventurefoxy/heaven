12d3712668279a25c105f96f9d3190bedb01424b
Adventure <mileshenrique@gmail.com>
2019-10-29 20:57:41 -0200

    Fixed the code and removed the type script

--- js/gamesheet.js
+++ js/gamesheet.js
@@ -1,8 +1,11 @@
-"use strict";
-exports.__esModule = true;
 function startop1() {
     var name = document.getElementById('name_input').value;
     if (name == '') {
         alert('Please, insert a name.');
+    } else {
+        document.getElementById('option1').style.display = 'none';
+        document.getElementById('option2').style.display = 'block';
+        document.getElementById('name').innerHTML = name;
     }
+
 }
--- js/gamesheet.ts
+++
@@ -1,6 +0,0 @@
-function startop1(){
-    let name:string = getElementById('name_input').value;
-    if (name == ''){
-        alert('Please, insert a name.');
-    }
-}
